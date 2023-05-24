072d9e22f703f2faeb5ef42b8612aeb23e71ce30
Olzhas Suleimen <amajisenpai@gmail.com>
2023-05-24 23:30:30 +0600

    Update assets path.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from "path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: '/packages/dartpad',
+  base: "/packages/dartpad",
   build: {
     emptyOutDir: false,
-    outDir: 'lib',
+    outDir: "lib",
     lib: {
-      entry: resolve(__dirname, 'src/dartpad.ts'),
-      name: 'dartpad',
+      entry: resolve(__dirname, "src/dartpad.ts"),
+      name: "dartpad",
       fileName: (format, entryName) => {
-        return `${entryName}.${format}.js`;
+        return `${entryName}.${format}.js`
       },
     },
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+        chunkFileNames: "assets/[name].js",
+      }
+    }
   },
+  worker: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+      }
+    }
+  }
 })
